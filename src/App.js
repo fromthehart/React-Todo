45c96830a22f8992c46c7aacff099dd5958bb299
fromthehart <fromthehart@pm.me>
2019-05-29 19:44:18 -0400

    Local storage broken. Needs to be fixed

--- src/App.js
+++ src/App.js
@@ -8,14 +8,28 @@ class App extends React.Component {
     super()
 
     // Try to load persisted todo list from local storage, or will be empty if not found
-    let saved = null
-    try {
-      saved = JSON.parse(localStorage.getItem('todos'))
-    } catch(e) {}
+    // let saved = JSON.parse(localStorage.getItem('todos'))
 
     // Initialize state with either our persisted data or empty
     this.state={
-      todos: saved,
+      // todos: saved,
+        [
+          {
+            task: 'Eat Breakfast',
+            id: 314159265358979323846,
+            completed: true
+          },
+          {
+            task: 'React Todo Day 1 MVP',
+            id: 1528817077286,
+            completed: false
+          },
+          {
+            task: 'React Todo Day 2 MVP',
+            id: 1528817084358,
+            completed: false
+          }
+        ],
       todo: ''
     }
     // console.log(this.state)
@@ -28,7 +42,7 @@ class App extends React.Component {
       id: Date.now(),
       completed: false
     }
-    if (todoEntry.task.trim() !== '') {
+    if (todoEntry.task.trim !== '') {
       this.setState({
         todos: [...this.state.todos, todoEntry],
         todo: ''
